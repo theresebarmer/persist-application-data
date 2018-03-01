7d92c4549cc7990d48b86ecc52cc68f81e922bba
Therése Barmer <therese.barmer@gmail.com>
2018-03-01 13:57:40 +0100

    Remove duplicates from array

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,31 +1,47 @@
-function loadItems() {
+let cart = document.getElementById("cart-items");
+let cartItems = JSON.parse(localStorage.getItem("cartItems"));
 
-    let cartItems = JSON.parse(localStorage.getItem("cartItems"));
-    let cart = document.getElementById("cart-items");
-
-    function displayCart(product) {
-        let productCard = document.createElement("article");
-        let heading = document.createElement("h3");
-        let description = document.createElement("p");
-        let price = document.createElement("footer");
-        let image = document.createElement("img");
-
-        heading.innerHTML = product.name;
-        image.setAttribute("src", product.image);
-        description.innerHTML = product.description;
-        price.innerHTML = `${product.price} SEK`;
-
-        productCard.appendChild(heading);
-        productCard.appendChild(image);
-        productCard.appendChild(description);
-        productCard.appendChild(price);
-        cart.appendChild(productCard);
+function calculateQuantity(product) {
+    let quantity = 0;
+    for(let item of cartItems) {
+        if(item.id === product.id) {
+            quantity++;
+        }
     }
+    return quantity;
+}
 
+function displayCart(product) {
+    let productCard = document.createElement("article");
+    let heading = document.createElement("h3");
+    let description = document.createElement("p");
+    let price = document.createElement("footer");
+    let image = document.createElement("img");
+    let quantity = document.createElement("span");
+
+    heading.innerHTML = product.name;
+    image.setAttribute("src", product.image);
+    description.innerHTML = product.description;
+    price.innerHTML = `${product.price} SEK`;
+    quantity.innerHTML = `${calculateQuantity(product)}`;
+
+    productCard.appendChild(heading);
+    productCard.appendChild(image);
+    productCard.appendChild(description);
+    productCard.appendChild(price);
+    productCard.appendChild(quantity);
+    cart.appendChild(productCard);
+}
+
+function loadItems() {
+    let deDuplicatedArray = cartItems.filter((product, index, cartItems) => {
+        return cartItems.map(object => {
+            return object["name"]}).indexOf(product["name"]) === index;
+    });
     cartItems === null
         ? cart.innerHTML = `<h2>No items added!</h2>`
-        : cartItems.forEach((item) => {
-            displayCart(item);
+        : deDuplicatedArray.forEach(function(item){
+            displayCart(item)
         });
 }
 loadItems();
